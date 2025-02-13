f3818c29c853792b88727d618bb4f6d51e6b7b92
Dario Massa <108331281+d-massa@users.noreply.github.com>
2025-02-13 11:50:10 +0100

    scripts and notebooks

---
+++ SolutionE.sh
@@ -0,0 +1,481 @@
+names[1]='Li'
+masses[1]=6.941
+PP[1]='Li.pbe-sl-rrkjus_psl.1.0.0.UPF'
+nspins[1]=1
+
+start_latt[1]=3.055 #(A) 
+
+names[2]='Na'
+masses[2]=22.9897
+PP[2]='Na.pbe-spnl-rrkjus_psl.1.0.0.UPF'
+nspins[2]=1
+
+start_latt[2]=3.715 #(A) 
+
+names[3]='Si'
+masses[3]=28.0855
+PP[3]='Si.pbe-nl-rrkjus_psl.1.0.0.UPF'
+nspins[3]=1
+
+start_latt[3]=2.736 #(A)
+
+names[4]='Ge'
+masses[4]=72.64
+PP[4]='Ge.pbe-dn-kjpaw_psl.1.0.0.UPF'
+nspins[4]=1
+
+start_latt[4]=3.029 #(A)
+
+names[5]='Rb'
+masses[5]=85.4678
+PP[5]='Rb.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[5]=1
+
+start_latt[5]=5.047 #(A) 
+
+names[6]='Sr'
+masses[6]=87.62
+PP[6]='Sr.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[6]=1
+
+start_latt[6]=4.259 #(A) 
+
+names[7]='Y'
+masses[7]=88.9059
+PP[7]='Y.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[7]=1
+
+start_latt[7]=3.580 #(A) 
+
+names[8]='Tc'
+masses[8]=98
+PP[8]='Tc.pbe-spn-rrkjus_psl.0.3.0.UPF'
+nspins[8]=1
+start_latt[8]=2.747 #(A)
+
+
+names[9]='Ru'
+masses[9]=101.07
+PP[9]='Ru.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[9]=1
+start_latt[9]=2.705  #(A)
+
+
+names[10]='Rh'
+masses[10]=102.9055
+PP[10]='Rh.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[10]=1
+start_latt[10]=2.718  #(A)
+
+
+names[11]='Pd'
+masses[11]=106.42
+PP[11]='Pd.pbe-n-rrkjus_psl.1.0.0.UPF'
+nspins[11]=1
+start_latt[11]=2.798 #(A)
+
+
+names[12]='Ag'
+masses[12]=107.8682
+PP[12]='Ag.pbe-n-rrkjus_psl.1.0.0.UPF'
+nspins[12]=1
+start_latt[12]=2.942 #(A)
+
+
+names[13]='Cd'
+masses[13]=112.411
+PP[13]='Cd.pbe-n-rrkjus_psl.1.0.0.UPF'
+nspins[13]=1
+start_latt[13]=3.204 #(A)
+
+
+names[14]='In'
+masses[14]=114.818
+PP[14]='In.pbe-dn-rrkjus_psl.1.0.0.UPF'
+nspins[14]=1
+start_latt[14]=3.391 #(A)
+
+
+names[15]='Sn'
+masses[15]=118.71
+PP[15]='Sn.pbe-dn-rrkjus_psl.1.0.0.UPF'
+nspins[15]=1
+start_latt[15]=3.404 #(A)
+
+
+names[16]='Sb'
+masses[16]=121.76
+PP[16]='Sb.pbe-n-rrkjus_psl.1.0.0.UPF'
+nspins[16]=1
+start_latt[16]=3.388 #(A)
+
+
+names[17]='Cs'
+masses[17]=132.9055
+PP[17]='Cs.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[17]=1
+start_latt[17]=5.458 #(A)
+
+
+names[18]='Ba'
+masses[18]=137.327
+PP[18]='Ba.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[18]=1
+start_latt[18]=4.491 #(A)
+
+
+names[19]='Ta'
+masses[19]=180.9479
+PP[19]='Ta.pbe-spfn-rrkjus_psl.1.0.0.UPF'
+nspins[19]=1
+start_latt[19]=2.989  #(A)
+
+
+names[20]='W'
+masses[20]=183.84
+PP[20]='W.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[20]=1
+start_latt[20]=2.859 #(A)
+
+
+names[21]='Re'
+masses[21]=186.207
+PP[21]='Re.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[21]=1
+start_latt[21]=2.775 #(A)
+
+
+names[22]='Os'
+masses[22]=190.23
+PP[22]='Os.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[22]=1
+start_latt[22]=2.732 #(A)
+
+
+names[23]='Ir'
+masses[23]=192.217
+PP[23]='Ir.pbe-n-rrkjus_psl.0.2.3.UPF'
+nspins[23]=1
+start_latt[23]=2.741 #(A)
+
+
+names[24]='Pt'
+masses[24]=195.078
+PP[24]='Pt.pbe-n-rrkjus_psl.1.0.0.UPF'
+nspins[24]=1
+start_latt[24]=2.812 #(A)
+
+
+names[25]='Au'
+masses[25]=196.9665
+PP[25]='Au.pbe-n-rrkjus_psl.1.0.0.UPF'
+nspins[25]=1
+start_latt[25]=2.950 #(A)
+
+names[26]='Hg'
+masses[26]=200.59
+PP[26]='Hg.pbe-n-rrkjus_psl.1.0.0.UPF'
+nspins[26]=1
+start_latt[26]=3.586 #(A)
+
+names[27]='Tl'
+masses[27]=204.3833
+PP[27]='Tl.pbe-dn-rrkjus_psl.1.0.0.UPF'
+nspins[27]=1
+start_latt[27]=3.531 #(A)
+
+names[28]='Pb'
+masses[28]=207.2
+PP[28]='Pb.pbe-dn-rrkjus_psl.1.0.0.UPF'
+nspins[28]=1
+start_latt[28]=3.571 #(A)
+
+names[29]='Mg'
+masses[29]=24.305
+PP[29]='Mg.pbe-spnl-rrkjus_psl.1.0.0.UPF'
+nspins[29]=1
+start_latt[29]=$(echo "scale=10; 8.588117428*0.529177249" |bc -l)
+
+names[30]='Al'
+masses[30]=26.98154
+PP[30]='Al.pbe-nl-rrkjus_psl.1.0.0.UPF'
+nspins[30]=1
+start_latt[30]=$(echo "scale=10; 8.062143314*0.529177249" |bc -l)
+
+names[31]='K'
+masses[31]=39.0983
+PP[31]='K.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[31]=1
+start_latt[31]=$(echo "scale=10; 10.54025566*0.529177249" |bc -l)
+
+names[32]='Ca'
+masses[32]=40.08
+PP[32]='Ca.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[32]=1
+start_latt[32]=$(echo "scale=10; 9.646404142*0.529177249" |bc -l)
+
+names[33]='Sc'
+masses[33]=44.9559
+PP[33]='Sc.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[33]=
+start_latt[33]=$(echo "scale=10; 8.578458738*0.529177249" |bc -l)
+
+names[34]='Ti'
+masses[34]=47.90
+PP[34]='Ti.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[34]=1
+start_latt[34]=$(echo "scale=10; 7.947784766*0.529177249" |bc -l)
+
+names[35]='V'
+masses[35]=50.9415
+PP[35]='V.pbe-spnl-rrkjus_psl.1.0.0.UPF'
+nspins[35]=1
+start_latt[35]=$(echo "scale=10; 7.496674402*0.529177249" |bc -l)
+
+names[36]='Cr'
+masses[36]=51.996
+PP[36]='Cr.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[36]=1
+start_latt[36]=$(echo "scale=10; 7.194187826*0.529177249" |bc -l)
+
+names[37]='Mn'
+masses[37]=54.9380
+PP[37]='Mn.pbe-spn-rrkjus_psl.0.3.1.UPF'
+nspins[37]=1
+start_latt[37]=$(echo "scale=10; 7.019749252*0.529177249" |bc -l)
+
+names[38]='Fe'
+masses[38]=55.847
+PP[38]='Fe.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[38]=2
+start_latt[38]=$(echo "scale=10; 7.204018478*0.529177249" |bc -l)
+
+names[39]='Co'
+masses[39]=58.9332
+PP[39]='Co.pbe-spn-rrkjus_psl.0.3.1.UPF'
+nspins[39]=2
+start_latt[39]=$(echo "scale=10; 7.043165940*0.529177249" |bc -l)
+
+names[40]='Ni'
+masses[40]=58.70
+PP[40]='Ni.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[40]=2
+start_latt[40]=$(echo "scale=10; 7.075412576*0.529177249" |bc -l)
+
+names[41]='Cu'
+masses[41]=63.546
+PP[41]='Cu.pbe-dn-rrkjus_psl.1.0.0.UPF'
+nspins[41]=1
+start_latt[41]=$(echo "scale=10; 7.403953036*0.529177249" |bc -l)
+
+names[42]='Zr'
+masses[42]=91.22
+PP[42]='Zr.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[42]=1
+start_latt[42]=$(echo "scale=10; 8.715598964*0.529177249" |bc -l)
+
+names[43]='Nb'
+masses[43]=92.9064
+PP[43]='Nb.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[43]=1
+start_latt[43]=$(echo "scale=10; 8.219086644*0.529177249" |bc -l)
+
+names[44]='Mo'
+masses[44]=95.94
+PP[44]='Mo.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[44]=1
+start_latt[44]=$(echo "scale=10; 7.865283380*0.529177249" |bc -l)
+
+names[45]='Hf'
+masses[45]=178.49
+PP[45]='Hf.pbe-spn-rrkjus_psl.1.0.0.UPF'
+nspins[45]=1
+start_latt[45]=$(echo "scale=10; 8.578071616*0.529177249" |bc -l)
+
+printf "%s/n" "#Elem    E_MH   E_M   hydrogen_binding_half   BE   "> SolutionE_OI.dat
+echo "\"Elem\",\"E_MH\",\"E_M\",\"hydrogen_binding_half\",\"BE\"" > SolutionE_OI.csv
+printf "%s/n" "#Elem    E_MH   E_M   hydrogen_binding_half   BE   "> SolutionE_TI.dat
+echo "\"Elem\",\"E_MH\",\"E_M\",\"hydrogen_binding_half\",\"BE\"" > SolutionE_TI.csv
+
+hydrogen_binding_half=-0.248344479 #−0.248344479 #−3.3789 eV
+
+nn=45
+#nn=1
+for (( ii=1 ; ii<=$nn ; ii++ )); 
+#for ii in 36
+do
+
+	inputdft='rvv10'
+	symmetry='FCC'
+	
+	name=${names[ii]}
+
+	echo ""
+	echo "--------------"
+	echo "OI: pure sample with defected structure"
+	echo $name
+	echo "--------------"
+	echo ""
+
+	if [[ "$inputdft" == "pbe" ]]
+	then  
+		namefolder=$name'_input_'$inputdft
+	else 
+		namefolder="$name"
+	fi
+
+	casefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/ALL_SCF_H_FILES_OCTA_CORRECT/'
+	tmpfolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/ALL_SCF_H_FILES_OCTA_CORRECT/tmp_'$namefolder
+
+	cd $casefolder
+#	mkdir $tmpfolder
+
+	#mpirun -np 8 /mnt/home/dmassa/q-e/bin/pw.x < $name.H.scf.in > $name.H.scf.out
+
+        echo ""
+        echo "--------------"
+        echo "OI: H only in the same empty cell"
+        echo $name
+        echo "--------------"
+        echo ""
+
+        casefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/H_ONLY_OCTA/'
+        tmpfolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/H_ONLY_OCTA/tmp_'$namefolder
+
+        cd $casefolder
+#        mkdir $tmpfolder
+
+        #mpirun -np 8 /mnt/home/dmassa/q-e/bin/pw.x < $name.H.scf.in > $name.H.scf.out
+
+
+        echo ""
+        echo "--------------"
+        echo "OI: COMPUTING THE BE"
+        echo $name
+        echo "--------------"
+        echo ""
+
+	filefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/'$name'/Density'
+	cd $filefolder
+	string=$(grep ! $name.H.scf.out)
+	E_MH=$(echo $string | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+	echo "The E_MH is"
+	echo $E_MH
+	echo ""
+
+	filefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/'$name'/'
+	cd $filefolder
+        string=$(grep ! $name.scf.out)
+        E_M=$(echo $string | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+        echo "The E_M is"
+        echo $E_M
+        echo ""
+
+        filefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/H_ONLY_OCTA/'
+        cd $filefolder
+        string=$(grep "!    total energy" $name.H.scf.out)
+        #hydrogen_binding_half=$(echo $string | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+        echo "The hydrogen_binding_half is"
+        echo $hydrogen_binding_half
+        echo ""
+
+	BE=$(echo "scale=4; $E_MH- $E_M- $hydrogen_binding_half" | bc -l)
+	#BE=$(echo "($E_MH-$E_M-$hydrogen_binding_half)" | bc -l )
+        echo "The BE is"
+        echo $BE
+        echo ""
+
+    	write_csv(){
+    	echo \"$1\",\"$2\",\"$3\",\"$4\",\"$5\" >> SolutionE_OI.csv
+   	 }
+
+   	write_csv $name $E_MH $E_M $hydrogen_binding_half $BE
+
+   	printf "%s " "$name" >> SolutionE_OI.dat 
+    	printf "%s " "$E_MH" >> SolutionE_OI.dat 
+    	printf "%s" "$E_M" >> SolutionE_OI.dat 
+        printf "%s" "$hydrogen_binding_half" >> SolutionE_OI.dat 
+    	printf "%s/n" "$ratio" >> SolutionE_OI.dat
+
+
+        echo ""
+        echo "--------------"
+        echo "TI: pure sample with defected structure"
+        echo $name
+        echo "--------------"
+        echo ""
+
+        if [[ "$inputdft" == "pbe" ]]
+        then
+                namefolder=$name'_input_'$inputdft
+        else
+                namefolder="$name"
+        fi
+
+        casefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/ALL_SCF_H_FILES_TETRA_CORRECT/'
+        tmpfolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/ALL_SCF_H_FILES_TETRA_CORRECT/tmp_'$namefolder
+
+        cd $casefolder
+#        mkdir $tmpfolder
+
+        #mpirun -np 8 /mnt/home/dmassa/q-e/bin/pw.x < $name.H.scf.in > $name.H.scf.out
+
+        echo ""
+        echo "--------------"
+        echo "TI: H only in the same empty cell"
+        echo $name
+        echo "--------------"
+        echo ""
+
+        casefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/H_ONLY_TETRA/'
+        tmpfolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/H_ONLY_TETRA/tmp_'$namefolder
+
+        cd $casefolder
+        #mkdir $tmpfolder
+
+        #mpirun -np 8 /mnt/home/dmassa/q-e/bin/pw.x < $name.H.scf.in > $name.H.scf.out
+
+
+        filefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/'$name'_tetra/Density'
+        cd $filefolder
+        string=$(grep ! $name.H.scf.out)
+        E_MH=$(echo $string | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+        echo "The E_MH is"
+        echo $E_MH
+        echo ""
+
+        filefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/'$name'_tetra/'
+        cd $filefolder
+        string=$(grep ! $name.scf.out)
+        E_M=$(echo $string | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+        echo "The E_M is"
+        echo $E_M
+        echo ""
+
+        filefolder='/mnt/home/dmassa/'$symmetry'/'$symmetry'_supercells/BE/H_ONLY_TETRA/'
+        cd $filefolder
+        string=$(grep "!    total energy" $name.H.scf.out)
+        #hydrogen_binding_half=$(echo $string | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+        echo "The hydrogen_binding_half is"
+        echo $hydrogen_binding_half
+        echo ""
+
+        BE=$(echo "scale=4; $E_MH- $E_M- $hydrogen_binding_half" | bc -l)
+	echo "The BE is"
+        echo $BE
+        echo ""
+
+        write_csv(){
+        echo \"$1\",\"$2\",\"$3\",\"$4\",\"$5\" >> SolutionE_TI.csv
+         }
+        
+        write_csv $name $E_MH $E_M $hydrogen_binding_half $BE
+
+        printf "%s " "$name" >> SolutionE_TI.dat
+        printf "%s " "$E_MH" >> SolutionE_TI.dat       
+        printf "%s" "$E_M" >> SolutionE_TI.dat            
+        printf "%s" "$hydrogen_binding_half" >> SolutionE_TI.dat
+        printf "%s/n" "$ratio" >> SolutionE_TI.dat
+
+
+done
